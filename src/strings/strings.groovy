661252a56a6f5d13d6438982db3511021bedfec1
rybicek <zjazvenyjazvec@gmail.com>
2018-09-04 19:17:23 +0200

    test

---
+++ src/strings/strings.groovy
@@ -0,0 +1,13 @@
+package strings
+
+class strings {
+    static void main(String[] args) {
+
+        println("--------------   Strings --------");
+        def name= "Derek";
+
+        println('I am ${name}\n');
+        println("I am ${name}\n");
+
+    }
+}
